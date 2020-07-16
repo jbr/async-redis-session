d5337c7cdf660dd458f063722f17c2ef6e272692
Jacob Rothstein <hi@jbr.me>
2020-07-15 19:43:35 -0700

    update for latest security-focused changes to async-session

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,12 @@
-use async_session::{uuid::Uuid, Session, SessionStore};
-use http_types::cookies::Cookie;
-use redis::AsyncCommands;
-use redis::{Client, RedisError};
+use async_session::{async_trait, base64, serde_json, utils, Session, SessionStore};
+use redis::{AsyncCommands, Client, RedisError};
 use std::time::Duration;
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct RedisSessionStore {
     client: Client,
     ttl: Duration,
+    prefix: Option<String>,
 }
 
 impl RedisSessionStore {
@@ -15,41 +14,74 @@ impl RedisSessionStore {
         Self {
             client,
             ttl: Duration::from_secs(86400),
+            prefix: None,
         }
     }
 
     pub fn new(connection_info: impl redis::IntoConnectionInfo) -> Result<Self, RedisError> {
         Ok(Self::from_client(Client::open(connection_info)?))
     }
+
+    pub fn with_prefix(mut self, prefix: String) -> Self {
+        self.prefix = Some(prefix);
+        self
+    }
+
+    pub fn with_ttl(mut self, ttl: Duration) -> Self {
+        self.ttl = ttl;
+        self
+    }
+
+    fn prefix_key(&self, key: impl AsRef<str>) -> String {
+        if let Some(ref prefix) = self.prefix {
+            format!("{}{}", prefix, key.as_ref())
+        } else {
+            key.as_ref().into()
+        }
+    }
+
+    async fn connection(&self) -> redis::RedisResult<redis::aio::Connection> {
+        self.client.get_async_std_connection().await
+    }
 }
 
-#[async_trait::async_trait]
+#[async_trait]
 impl SessionStore for RedisSessionStore {
     type Error = Error;
 
-    async fn load_session(&self, cookie: Cookie<'_>) -> Result<Option<Session>, Self::Error> {
-        let mut connection = self.client.get_async_std_connection().await?;
-        let value: String = connection.get(cookie.value()).await?;
-        let session: Session = serde_json::from_str(&value)?;
-        Ok(Some(session))
+    async fn load_session(&self, cookie_value: String) -> Option<Session> {
+        let id = utils::id_from_string(&cookie_value).ok()?;
+        let mut connection = self.connection().await.ok()?;
+        match connection.get::<_, Option<String>>(id).await.ok()? {
+            Some(value) => serde_json::from_str(&value).ok()?,
+            None => None,
+        }
     }
 
-    async fn store_session(&self, session: Session) -> Result<String, Self::Error> {
+    async fn store_session(&self, mut session: Session) -> Option<String> {
         let id = session.id();
-        let mut connection = self.client.get_async_std_connection().await?;
-        let string = serde_json::to_string(&session)?;
+        let string = serde_json::to_string(&session).ok()?;
 
-        let _: () = connection
-            .set_ex(&id, string, self.ttl.as_secs() as usize)
-            .await?;
+        let mut connection = self.connection().await.ok()?;
+        connection
+            .set_ex::<_, _, ()>(id, string, self.ttl.as_secs() as usize)
+            .await
+            .ok()?;
 
-        Ok(id)
+        session.take_cookie_value()
     }
 
-    async fn create_session(&self) -> Result<Session, Self::Error> {
-        let sess = Session::new();
-        sess.insert("id".to_string(), Uuid::new_v4().to_string());
-        Ok(sess)
+    async fn destroy_session(&self, session: Session) -> Result<(), Self::Error> {
+        self.connection()
+            .await?
+            .del::<_, ()>(self.prefix_key(session.id().to_string()))
+            .await?;
+        Ok(())
+    }
+
+    async fn clear_store(&self) -> Result<(), Self::Error> {
+        self.connection().await?.del(self.prefix_key("*")).await?;
+        Ok(())
     }
 }
 
@@ -57,6 +89,7 @@ impl SessionStore for RedisSessionStore {
 pub enum Error {
     RedisError(RedisError),
     SerdeError(serde_json::Error),
+    Base64Error(base64::DecodeError),
 }
 
 impl std::fmt::Display for Error {
@@ -64,6 +97,7 @@ impl std::fmt::Display for Error {
         match self {
             Error::RedisError(e) => e.fmt(f),
             Error::SerdeError(e) => e.fmt(f),
+            Error::Base64Error(e) => e.fmt(f),
         }
     }
 }
@@ -71,6 +105,12 @@ impl std::fmt::Display for Error {
 impl From<serde_json::Error> for Error {
     fn from(e: serde_json::Error) -> Self {
         Self::SerdeError(e)
+    }
+}
+
+impl From<base64::DecodeError> for Error {
+    fn from(e: base64::DecodeError) -> Self {
+        Self::Base64Error(e)
     }
 }
 
